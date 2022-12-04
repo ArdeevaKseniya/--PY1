1a3360ced98df547fdb0f32c996fe5e275dbaefe
ArdeevaKseniya <119007862+ArdeevaKseniya@users.noreply.github.com>
2022-12-04 17:09:14 +0300

    Лабораторная работа 4

---
+++ task_2.py
@@ -0,0 +1,14 @@
+def delete(list_, index=None):
+
+    if index is None:
+        index = list_[-1]
+
+    left_side_not_including_index = list_[:index]
+    right_side_not_including_index = list_[index+1:]
+
+    return left_side_not_including_index + right_side_not_including_index
+
+print(delete([0, 1, 2], index=0))  # [1, 2]
+print(delete([0, 1, 2], index=1))  # [0, 2]
+print(delete([0, 1, 2]))  # [0, 1]
+
